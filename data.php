44f261e5f2403d7f6bf5d4a6b7a5170b0370d26d
AEARIAL <koki818325@icloud.com>
2024-11-27 21:24:06 +0900

    test

---
+++ data.php
@@ -0,0 +1,34 @@
+<?php
+$games = [
+    [
+        'title' => 'VEGORA ENDLESS',
+        'description' => 'このゲームはロボットの「helper」が様々な場所の放棄されたロボットを倒し、部品を集め、自身の強化をしながら、危険地帯を進んでいくゲームです。あなたは、果たしなく続く危険地帯にどれだけ立ち向かうことができますか？',
+        'genre' => '2D/シングルプレイ/アクション',
+        'link' => 'VE/index.html',
+        'version' => 'v02',
+    ],
+    [
+        'title' => 'Key Or Trap (新)',
+        'description' => '改修中の新バージョンのボードゲーム。',
+        'genre' => '2D/マルチプレイ/ボードゲーム',
+        'link' => 'KOT/index.html',
+        'rule_link' => 'kot_rule.html',
+        'cost_tool' => 'https://aearial.github.io/my-app/',
+        'version' => 'vb01',
+    ],
+    [
+        'title' => 'Key Or Trap (旧)',
+        'description' => 'マスの効果を自由に配置することができるマルチプレイすごろくゲーム。',
+        'genre' => '2D/マルチプレイ/ボードゲーム',
+        'link' => 'KOTBCC/index.html',
+        'rule_link' => 'old_kot_rule.html',
+        'version' => 'v21',
+    ]
+];
+
+$changelog = [
+    'v21' => ['解読機の削除'],
+    'v20' => ['解読機&スキル調整'],
+    'v19' => ['アドオンアイテム改善', '基本移動がダイスから1～3マスの確定移動へ'],
+    // 他のバージョンの修正項目を追加...
+  ];
---
+++ footer.php
@@ -0,0 +1,5 @@
+<footer>
+    <p>© 2024 AEARIAL. This work is licensed under a Creative Commons Attribution 4.0 International License (CC BY 4.0).</p>
+</footer>
+</body>
+</html>
